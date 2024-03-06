a28b937175c7a4e03825fc8551200f9241c3acea
EslamElsebaey <eslamelsebaey.11@gmail.com>
2024-03-06 13:14:20 +0200

    add slider to main section

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
         // main Swiper 
 const mainSwiper = new Swiper('.mainBanner .swiper', {
   loop: true,
-  //  autoplay: true,
+   autoplay: true,
   draggable: true,
   navigation: {
     nextEl: ' .mainBanner .swiper-button-next',
